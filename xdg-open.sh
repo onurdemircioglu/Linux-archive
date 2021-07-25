69997f9e5f63dc0007f53406c6c3bdd4fd8c4426
Onur Demircioğlu <59303053+onurdemircioglu@users.noreply.github.com>
2021-07-25 09:19:41 +0300

    Create xdg-open.sh

---
+++ xdg-open.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+xdg-open / >> opens File System folder
+xdg-open /home/ >> opens home directory
+xdg-open TEMP.ods >> open TEMP.ods with LibreOffice Calc (or user's preferred (default) application)
+xdg-open "https://www.google.com/" >> opens www.google.com with default browser
+
+
+#man page >> https://linux.die.net/man/1/xdg-open
+
+#Notes
+#OS >> Linux Mint 19.3 Cinnamon
+#bash version >> GNU bash, version 4.4.20(1)-release (x86_64-pc-linux-gnu)
