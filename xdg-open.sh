44f4464ce4e59b4bb5c273402dcd0140b9ff5a02
Onur Demircioğlu <59303053+onurdemircioglu@users.noreply.github.com>
2021-07-25 10:29:38 +0300

    Update xdg-open.sh

--- xdg-open.sh
+++ xdg-open.sh
@@ -2,6 +2,7 @@
 
 xdg-open / >> opens File System folder
 xdg-open /home/ >> opens home directory
+xdg-open . >> open current working directory
 xdg-open TEMP.ods >> open TEMP.ods with LibreOffice Calc (or user's preferred (default) application)
 xdg-open "https://www.google.com/" >> opens www.google.com with default browser
 
